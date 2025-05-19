d4a9e007a60f8cefc6ce1256dc13db9444f0eaae
Divyansh <142193842+csdivyansh@users.noreply.github.com>
2025-05-19 21:25:17 +0530

    Update TodoInput.jsx
    Enabled enter to add todo

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -5,18 +5,21 @@ export default function TodoInput(props) {
     <header>
       <input
         value={todoValue}
-        onChange={(e) => {
-          setTodoValue(e.target.value);
+        onChange={(e) => setTodoValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            document.getElementById("add-btn").click();
+          }
         }}
         placeholder="Enter todo..."
       />
       <button
+        id="add-btn"
         onClick={() => {
           handleAddTodos(todoValue);
           setTodoValue("");
         }}
-      >
-        Add
+      >Add
       </button>
     </header>
   );
